a95171fcaf935e0b62c84de954138c149060656d
VonZalius <ad.barras@bluewin.ch>
2023-08-28 21:39:20 +0200

    v4

--- minishell.c
+++ minishell.c
@@ -239,7 +239,7 @@ int	cmd_in_struct(lexer *word, char *cmd, int start)
 			while(cmd[word->i] != ' ' && cmd[word->i] != '\0' && cmd[word->i] != '|' && cmd[word->i] != '<' && cmd[word->i] != '>')
 				word->i++;
 		}
-		else if (cmd[word->i] == '-' && word->cmd_check != 0)
+		else if (cmd[word->i] != ' ' && cmd[word->i] != '\0' && cmd[word->i] != '|' && cmd[word->i] != '\"'  && cmd[word->i] != '\'' && cmd[word->i] != '<' && cmd[word->i] != '>' && word->cmd_check != 0)
 		{
 			//word->i++;
 			//i_bis++;
@@ -298,7 +298,7 @@ int	cmd_in_struct(lexer *word, char *cmd, int start)
 			word->i++;
 			index_arg++;
 		}
-		else if (cmd[word->i] != ' ' && cmd[word->i] != '\0' && cmd[word->i] != '|' && cmd[word->i] != '-' && cmd[word->i] != '\"'  && cmd[word->i] != '\'' && cmd[word->i] != '<' && cmd[word->i] != '>')
+		/*else if (cmd[word->i] != ' ' && cmd[word->i] != '\0' && cmd[word->i] != '|' && cmd[word->i] != '-' && cmd[word->i] != '\"'  && cmd[word->i] != '\'' && cmd[word->i] != '<' && cmd[word->i] != '>')
 		{  
 			if (word->cmd_check != 0)
 			{
@@ -319,7 +319,7 @@ int	cmd_in_struct(lexer *word, char *cmd, int start)
 				index_arg++;
 				word->cmd_check++;
 			}
-			/*
+			
 			else
 			{
 				while (cmd[i_bis] != ' ' && cmd[i_bis] != '\0' && cmd[i_bis] != '|' && cmd[i_bis] != '<' && cmd[i_bis] != '>')
@@ -336,8 +336,8 @@ int	cmd_in_struct(lexer *word, char *cmd, int start)
 				}
 				word->word[i_bis] = '\0';
 				word->cmd_check = 1;
-			}*/
-		}
+			}
+		}*/
 		else
 			word->i++;
 	}
@@ -367,7 +367,7 @@ void	ft_free_lexer(lexer *word, char *cmd)
 	free (cmd);
 }
 
-void    execute_echo(char **args)
+/*void    execute_echo(char **args)
 {
     int    i;
     int    add_newline;
@@ -389,6 +389,123 @@ void    execute_echo(char **args)
     }
     if (add_newline)
         printf("\n");
+}*/
+
+int ft_len_of_$(char *str1)
+{
+	int	i;
+	int	b;
+
+	i = 0;
+	b = 0;
+	while (str1[i] != '\0')
+	{
+		if(str1[i] == '$')
+		{
+			i++;
+			while(str1[i] != ' ' && str1[i] != '\0' && str1[i] != '"' && str1[i] != '|' && str1[i] != '<' && str1[i] != '>' && str1[i] != '$')
+			{
+				b++;
+				i++;
+			}
+			return (b);
+		}
+		i++;
+	}
+	return (b);
+}
+
+char	*ft_strinsert(char *str1, char *str2, int pos)
+{
+	char	*final;
+	int		len;
+	int		i;
+	int		j;
+	int		k;
+
+	if (str2 == NULL)
+		return (NULL);
+	len = ft_strlen(str1) + ft_strlen(str2) - ft_len_of_$(str1);
+	printf ("SALUUUT %s %li %li %i\n",str1 ,ft_strlen(str1), ft_strlen(str2), ft_len_of_$(str1));
+	final = malloc (sizeof(char) * (len) + 1);
+	if (final == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	k = 0;
+	while(i != len)
+	{
+		if (i != pos)
+		{
+			final[i] = str1[j];
+			j++;
+			i++;
+		}
+		else
+		{
+			j++;
+			while (str2[k] != '\0')
+			{
+				final[i] = str2[k];
+				k++;
+				i++;
+			}
+			while (str1[j] != ' ' && str1[j] != '\0' && str1[j] != '"' && str1[j] != '|' && str1[j] != '<' && str1[j] != '>' && str1[j] != '$')
+				j++;
+		}
+	}
+	free (str1);
+	final[i] = '\0';
+	return (final);
+}
+
+size_t	ft_strlen_space(const char *s, size_t i)
+{
+	while (s[i] != ' ' && s[i] != '\0' && s[i] != '|' && s[i] != '<' && s[i] != '>' && s[i] != '$')
+		i++;
+	return (i);
+}
+
+char	*search_for_env(lexer *word, char *cmd)
+{
+	char	*str2;
+	int		i;
+	int		j;
+
+	word->i = 0;
+	while (cmd[word->i] != '\0' && cmd[word->i] != '|')
+	{
+		i = 0;
+		if (cmd[word->i] == '\'')
+		{
+			word->i++;
+			while (cmd[word->i] != '\'')
+			{
+				if (cmd[word->i] == '\0')
+					return (NULL);
+				word->i++;
+			}
+		}
+		if (cmd[word->i] == '$')
+		{
+			word->i++;
+			j = word->i;
+			str2 = malloc(sizeof(char) * (ft_strlen_space(cmd, word->i) + 1));
+			while (cmd[j] != '\0' && cmd[j] != ' ' && cmd[j] != '"' && cmd[j] != '|' && cmd[j] != '<' && cmd[j] != '>' && cmd[j] != '$')
+			{
+				str2[i] = cmd[j];
+				i++;
+				j++;
+			}
+			str2[i] = '\0';
+			cmd = ft_strinsert(cmd, getenv(str2), word->i - 1);
+			if (cmd == NULL)
+				return (NULL);
+		}
+		word->i++;
+	}
+	//free (str2);
+	return (cmd);
 }
 
 int main(void)
@@ -443,8 +560,14 @@ int main(void)
 				return (0);
 			}
 			printf("Fdwrite done !\n");
-		//TEST TEST TEST A RECHECKER 	SUPPRIME LES " ET LES '
-			
+		//TEST TEST TEST A RECHECKER 	Remplace les $ par l'environnement
+			cmd = search_for_env(word, cmd);
+			if (cmd == NULL)
+			{
+				printf("We got a problem with the environnement bro !\n");
+				return (0);
+			}
+			printf("Environnement done !\n");
 		//Last check across the cmd
 			is_pipe = cmd_in_struct(word, cmd, start);
 			if (is_pipe == 0)
@@ -463,12 +586,12 @@ int main(void)
 			}
 		}
 
-		if (strcmp(word->word, "echo") == 0)
+		/*if (strcmp(word->word, "echo") == 0)
 		{
 			printf("NIKTARACE");
             execute_echo(word->arg);
             //continue ;
-        }
+        }*/
 
 		printf("\nYou said : %s  <--------------------------------------\n\n", cmd);
 		j--;
@@ -503,3 +626,4 @@ int main(void)
 }
 
 //save validé (oui oui)!
+// GERE LE PIPE POUR LES < > $
